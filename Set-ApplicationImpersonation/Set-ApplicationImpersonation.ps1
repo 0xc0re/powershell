57e7361d0b4e25889c7c097d074627fd980e60f9
John Gruber <john.gruber@tierpoint.com>
2017-09-26 16:25:52 -0400

    Added Office 365 impersonation Config

---
+++ Set-ApplicationImpersonation/Set-ApplicationImpersonation.ps1
@@ -0,0 +1,6 @@
+﻿$AdminEmail = ""
+
+​Enable-OrganizationCustomization
+New-ManagementRoleAssignment -Role ApplicationImpersonation -User $AdminEmail
+Set-MsolUser -UserPrincipalName $AdminEmail -PasswordNeverExpires $true
+​

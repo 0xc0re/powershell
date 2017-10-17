34f29587da53aec4e98407207c70803f5ad6db42
John Gruber <gruberjl@gmail.com>
2017-10-17 14:16:17 -0400

    added basic mailbox folder permission script

---
+++ Add-MailboxFolderPermission/Add-MailboxFolderPermission.ps1
@@ -0,0 +1,12 @@
+﻿<#
+
+.LINK
+https://technet.microsoft.com/en-us/library/dd298062(v=exchg.160).aspx
+#>
+
+$Permission = "PublishingEditor"
+$GrantToUser = ""
+$DestinationMailbox = ""
+$Folder = "Calendar"
+
+Add-MailboxFolderPermission -Identity "$($DestinationMailbox):\$($Folder)" -User $GrantToUser -AccessRights $Permission

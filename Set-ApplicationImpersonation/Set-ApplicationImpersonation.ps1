68b0d061c3ca29bb761316d385444538d55bdc71
John Gruber <gruberjl@gmail.com>
2017-10-17 13:56:15 -0400

    updated scripts to include timezone

--- Set-ApplicationImpersonation/Set-ApplicationImpersonation.ps1
+++ Set-ApplicationImpersonation/Set-ApplicationImpersonation.ps1
@@ -1,5 +1,7 @@
 ﻿$AdminEmail = ""
 
+Set-MailboxRegionalConfiguration -Identity $AdminEmail -Language 1033 -TimeZone "Eastern Standard Time"
+
 ​Enable-OrganizationCustomization
 New-ManagementRoleAssignment -Role ApplicationImpersonation -User $AdminEmail
 Set-MsolUser -UserPrincipalName $AdminEmail -PasswordNeverExpires $true

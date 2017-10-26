d14b8ba63f46ac4900909d1ffb4f7cb937b98d56
John Gruber <gruberjl@gmail.com>
2017-10-26 08:49:59 -0400

    added script and docs

---
+++ Hide-AllCompanyContacts/Hide-AllCompanyContacts.ps1
@@ -0,0 +1,26 @@
+﻿<#
+.SYNOPSIS
+Hide all company contacts from the GAL
+
+.DESCRIPTION
+Exports the list of company contacts to a spreadsheet documenting them before the change.
+Marks all company contacts as hidden.
+
+.NOTES
+connect to Office 365 powershell
+change directory to a location for the output
+Run the script
+wait 24 hours or run Update-AddressBooks.ps1
+
+.LINK
+https://github.com/gruberjl/powershell/tree/master/connect
+https://technet.microsoft.com/en-us/library/bb124717(v=exchg.160).aspx
+https://technet.microsoft.com/en-us/library/aa995950(v=exchg.160).aspx
+https://github.com/gruberjl/powershell/tree/master/Update-AddressBooks
+#>
+
+$Contacts = Get-MailContact -ResultSize unlimited
+
+$Contacts | Export-Csv "Mail-Contacts-Before-Change.csv" -NoTypeInformation
+
+$Contacts | Set-MailContact -HiddenFromAddressListsEnabled $true

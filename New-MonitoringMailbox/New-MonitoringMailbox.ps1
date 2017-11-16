806ef05a936524356c6c559d8e31e931f21656bf
John Gruber <gruberjl@gmail.com>
2017-11-16 10:24:45 -0500

    Added notes and hide mailbox from address list

--- New-MonitoringMailbox/New-MonitoringMailbox.ps1
+++ New-MonitoringMailbox/New-MonitoringMailbox.ps1
@@ -12,12 +12,13 @@ $cred = New-Object -typename System.Management.Automation.PSCredential -argument
 Send-MailMessage -To "email@address.com" -From 'Room.mailbox@email.com' -Subject "w/e" -Body 'w/e' -Credential $cred -SmtpServer "smtp.office365.com" -usessl
 
 .NOTES
-Version: 0.01
+Version: 0.02
 
 .LINK
 New-Mailbox: https://technet.microsoft.com/en-us/library/aa997663(v=exchg.160).aspx
 Set-MailboxJunkEmailConfiguration: https://technet.microsoft.com/en-us/library/dd979780(v=exchg.160).aspx
 Set-MsolUser: https://docs.microsoft.com/en-us/powershell/module/msonline/set-msoluser?view=azureadps-1.0
+Set-Mailbox: https://technet.microsoft.com/en-us/library/bb123981(v=exchg.160).aspx
 #>
 
 Param(
@@ -35,10 +36,21 @@ Param(
 
 )
 
+# Create Mailbox
 $EncryptedPassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
 $Mbx = New-Mailbox -Alias $Alias -Name $Name -Room -EnableRoomMailboxAccount $true -RoomMailboxPassword $EncryptedPassword
+
+# Disable Junk email filter
 $Mbx | Set-MailboxJunkEmailConfiguration –Enabled $False
+
+# Set Password to never expire
 Set-MsolUser -UserPrincipalName $Mbx.UserPrincipalName -PasswordNeverExpires $true
 
+# Hide mailbox from address books
+Set-Mailbox $Mbx.UserPrincipalName -HiddenFromAddressListsEnabled $true
+
+# Set the company. This should be set to the customer runnning the script if another tenant is sending the email. For example, If I run a script from the TierPoint tenant for our customer MTE then I set the company to MTE
 if ($Company) { Set-User $Mbx.UserPrincipalName -Company $Company }
+
+# Notes about why the mailbox is used.
 if ($Notes) { Set-User $Mbx.UserPrincipalName -Notes $Notes }
